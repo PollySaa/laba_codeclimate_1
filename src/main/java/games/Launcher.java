972c823900a6128dee39f8a655b28ed3a43f2972
pollysa <pollysaa@yandex.ru>
2025-03-17 11:41:26 +0300

    6 test

--- src/main/java/games/Launcher.java
+++ src/main/java/games/Launcher.java
@@ -1,14 +1,13 @@
 package games;
 
 import games.engine.GameEngine;
-
 import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число - 1, если хотите запустить игру 'Geometric Progression Game'" +
-                "Введите число - 2, если хотите запустить игру 'LCM Game': ");
+                "\nВведите число - 2, если хотите запустить игру 'LCM Game': ");
         if (scanner.hasNextInt()) {
             int number = scanner.nextInt();
             if (number == 1) {
@@ -18,7 +17,8 @@ public class Launcher {
                 gameEngine.startGame(scanner, new GeometricProgressionGame());
             } else if (number == 2) {
                 scanner = new Scanner(System.in);
-                GameEngine gameEngine = new GameEngine("LCM Game", "Find the smallest common multiple of given numbers.", 3);
+                GameEngine gameEngine = new GameEngine("LCM Game",
+                        "Find the smallest common multiple of given numbers.", 3);
                 gameEngine.startGame(scanner, new LcmGame());
             } else {
                 System.out.println("Неверная цифра!");
